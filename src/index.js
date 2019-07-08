0ab8d27338389fbf00a9803d86bbabd2dc137740
MagicMarvMan <marvin.kai@gmx.de>
2019-07-08 09:13:21 +0200

    Add basic http request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 const chalk = require("chalk")
+const request = require("request")
 const sslCertificate = require("get-ssl-certificate")
+
+// set colored logger functions
+const error = (err) => {
+	console.log(chalk.bgRed.black("ERR!") + " " + err)
+}
+
+const success = (msg) => {
+	console.log(chalk.bgGreen.black("SUC!") + " " + msg)
+}
+
+const warn = (msg) => {
+	console.log(chalk.bgYellow.black("WARN!") + " " + msg)
+}
+
+
+// set response variable to null
+let response = null
+
+
+// request website and load response to variable
+request
+	.get("https://schoolstats.de")
+	.on('response', (resp) => {
+		success("HTTP request successful.")
+	})
+	.on('error', (err) => {
+		error('HTTP request failed.')
+	})
+
+
